e74ffd9d0424efcf1cb6b6a538366e2de983488f
Alan.Chang <Alan.Chang@bionime.com>
2019-05-26 01:01:44 +0800

    實作 FizzBuzz checkNumber() 功能

--- FizzBuzz/src/main/java/FizzBuzz.java
+++ FizzBuzz/src/main/java/FizzBuzz.java
@@ -4,6 +4,16 @@ public class FizzBuzz {
     }
 
     public String checkNumber(int num) {
-        return "";
+        String result;
+        if (num % 3 == 0 && num % 5 == 0) {
+            result = "FizzBuzz";
+        } else if (num % 3 == 0) {
+            result = "Fizz";
+        } else if (num % 5 == 0) {
+            result = "Buzz";
+        } else {
+            result = String.valueOf(num);
+        }
+        return result;
     }
 }

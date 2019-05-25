8aa06c294812266f63199194d8791569863d4235
Alan.Chang <Alan.Chang@bionime.com>
2019-05-26 01:24:53 +0800

    針對 FizzBuzz 第二階段需求變更設計測試案例

--- FizzBuzz/src/test/java/FizzBuzzTest.java
+++ FizzBuzz/src/test/java/FizzBuzzTest.java
@@ -17,13 +17,19 @@ public class FizzBuzzTest {
         /*
             測試案例
             1. 3 的倍數要顯示 Fizz
-            2. 5 的倍數要顯示 Buzz
-            3. 3 % 5 的倍數要顯示 FizzBuzz
-            4. 其餘顯示該數字
+            2. 數字包含 3 要顯示 Fizz
+            3. 5 的倍數要顯示 Buzz
+            4. 數字包含 5 要顯示 Buzz
+            5. 3 % 5 的倍數要顯示 FizzBuzz
+            6. 其餘顯示該數字
          */
-        assertEquals("Fizz",fizzBuzz.checkNumber(3));
-        assertEquals("Buzz",fizzBuzz.checkNumber(5));
-        assertEquals("FizzBuzz",fizzBuzz.checkNumber(15));
-        assertEquals("1",fizzBuzz.checkNumber(1));
+        assertEquals("Fizz", fizzBuzz.checkNumber(3));
+        assertEquals("Fizz", fizzBuzz.checkNumber(13));
+        assertEquals("Fizz", fizzBuzz.checkNumber(31));
+        assertEquals("Buzz", fizzBuzz.checkNumber(5));
+        assertEquals("Buzz", fizzBuzz.checkNumber(10));
+        assertEquals("Buzz", fizzBuzz.checkNumber(52));
+        assertEquals("FizzBuzz", fizzBuzz.checkNumber(15));
+        assertEquals("1", fizzBuzz.checkNumber(1));
     }
 }

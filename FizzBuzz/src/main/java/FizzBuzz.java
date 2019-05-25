84c837de672a1e3600aac001c96bb1226291ece5
Alan.Chang <Alan.Chang@bionime.com>
2019-05-26 01:05:21 +0800

    實作主任務結果

--- FizzBuzz/src/main/java/FizzBuzz.java
+++ FizzBuzz/src/main/java/FizzBuzz.java
@@ -1,6 +1,9 @@
 public class FizzBuzz {
     public static void main(String[] args) {
-
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        for (int i = 0; i < 100; i++) {
+            System.out.println(fizzBuzz.checkNumber(i + 1));
+        }
     }
 
     public String checkNumber(int num) {

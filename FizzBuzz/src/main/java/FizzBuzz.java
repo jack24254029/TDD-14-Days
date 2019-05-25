7b5467da7f2ba9995904d863604c5c304e127a76
Alan.Chang <Alan.Chang@bionime.com>
2019-05-26 01:17:33 +0800

    根據其他學員交流後，TDD 只需通過 Test 即可。

--- FizzBuzz/src/main/java/FizzBuzz.java
+++ FizzBuzz/src/main/java/FizzBuzz.java
@@ -1,10 +1,4 @@
 public class FizzBuzz {
-    public static void main(String[] args) {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        for (int i = 0; i < 100; i++) {
-            System.out.println(fizzBuzz.checkNumber(i + 1));
-        }
-    }
 
     public String checkNumber(int num) {
         String result;

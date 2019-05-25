0ffa95a4771b6bcec33de412eabbee402511617b
Alan.Chang <Alan.Chang@bionime.com>
2019-05-26 01:25:47 +0800

    為了通過測試案例，修改 FizzBuzz checkNumber()

--- FizzBuzz/src/main/java/FizzBuzz.java
+++ FizzBuzz/src/main/java/FizzBuzz.java
@@ -4,9 +4,9 @@ public class FizzBuzz {
         String result;
         if (num % 3 == 0 && num % 5 == 0) {
             result = "FizzBuzz";
-        } else if (num % 3 == 0) {
+        } else if (num % 3 == 0 || String.valueOf(num).contains("3")) {
             result = "Fizz";
-        } else if (num % 5 == 0) {
+        } else if (num % 5 == 0 || String.valueOf(num).contains("5")) {
             result = "Buzz";
         } else {
             result = String.valueOf(num);

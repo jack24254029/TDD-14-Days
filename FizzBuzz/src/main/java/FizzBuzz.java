1c2d18a18d2a369e1e2626572e69a21dbadc6dfa
Shun-Min Chang <ji394m6y7@gmail.com>
2019-05-27 00:00:31 +0800

    實作需求變更的方法

--- FizzBuzz/src/main/java/FizzBuzz.java
+++ FizzBuzz/src/main/java/FizzBuzz.java
@@ -1,11 +1,11 @@
 public class FizzBuzz {
     public String checkNumberIsFizz(int num) {
-        if (num % 3 == 0) return "Fizz";
+        if (num % 3 == 0 || String.valueOf(num).contains("3")) return "Fizz";
         return String.valueOf(num);
     }
 
     public String checkNumberIsBuzz(int num) {
-        if (num % 5 == 0) return "Buzz";
+        if (num % 5 == 0 || String.valueOf(num).contains("5")) return "Buzz";
         return String.valueOf(num);
     }
 

eae8a601e256066be779366aab139034bde974c2
Shun-Min Chang <ji394m6y7@gmail.com>
2019-05-28 23:44:03 +0800

    初始化專案

--- FizzBuzz/src/main/java/FizzBuzz.java
+++ FizzBuzz/src/main/java/FizzBuzz.java
@@ -1,16 +1,3 @@
 public class FizzBuzz {
-    public String checkNumberIsFizz(int num) {
-        if (num % 3 == 0 || String.valueOf(num).contains("3")) return "Fizz";
-        return String.valueOf(num);
-    }
-
-    public String checkNumberIsBuzz(int num) {
-        if (num % 5 == 0 || String.valueOf(num).contains("5")) return "Buzz";
-        return String.valueOf(num);
-    }
-
-    public String checkNumberIsFizzBuzz(int num) {
-        if (num % 15 == 0) return "FizzBuzz";
-        return String.valueOf(num);
-    }
+    
 }
--- FizzBuzz/src/test/java/FizzBuzzTest.java
+++ FizzBuzz/src/test/java/FizzBuzzTest.java
@@ -1,44 +1,4 @@
-import org.junit.Before;
-import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
-
 public class FizzBuzzTest {
 
-    private FizzBuzz fizzBuzz;
 
-    @Before
-    public void setUp() {
-        fizzBuzz = new FizzBuzz();
-    }
-
-    @Test
-    public void checkNumberIsFizz() {
-        assertEquals("Fizz", fizzBuzz.checkNumberIsFizz(3));
-        assertEquals("Fizz", fizzBuzz.checkNumberIsFizz(6));
-        assertEquals("Fizz", fizzBuzz.checkNumberIsFizz(9));
-        assertEquals("Fizz", fizzBuzz.checkNumberIsFizz(31));
-        assertEquals("Fizz", fizzBuzz.checkNumberIsFizz(32));
-        assertEquals("Fizz", fizzBuzz.checkNumberIsFizz(33));
-        assertEquals("1", fizzBuzz.checkNumberIsFizz(1));
-    }
-
-    @Test
-    public void checkNumberIsBuzz() {
-        assertEquals("Buzz", fizzBuzz.checkNumberIsBuzz(5));
-        assertEquals("Buzz", fizzBuzz.checkNumberIsBuzz(10));
-        assertEquals("Buzz", fizzBuzz.checkNumberIsBuzz(20));
-        assertEquals("Buzz", fizzBuzz.checkNumberIsBuzz(51));
-        assertEquals("Buzz", fizzBuzz.checkNumberIsBuzz(52));
-        assertEquals("Buzz", fizzBuzz.checkNumberIsBuzz(53));
-        assertEquals("1", fizzBuzz.checkNumberIsBuzz(1));
-    }
-
-    @Test
-    public void checkNumberIsFizzBuzz() {
-        assertEquals("FizzBuzz", fizzBuzz.checkNumberIsFizzBuzz(15));
-        assertEquals("FizzBuzz", fizzBuzz.checkNumberIsFizzBuzz(30));
-        assertEquals("FizzBuzz", fizzBuzz.checkNumberIsFizzBuzz(45));
-        assertEquals("1", fizzBuzz.checkNumberIsBuzz(1));
-    }
 }
